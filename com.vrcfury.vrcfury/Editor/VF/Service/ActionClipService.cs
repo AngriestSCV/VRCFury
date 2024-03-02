0c6e6ff7878a3f858df530cda4ab584947fcbae6
AngriestSCV <112354326+AngriestSCV@users.noreply.github.com>
2024-03-02 12:46:12 -0600

    Adding warning for animating shader keywords
    Added warning for shader keyword property lookup since they can't be used for toggle creation.

--- com.vrcfury.vrcfury/Editor/VF/Service/ActionClipService.cs
+++ com.vrcfury.vrcfury/Editor/VF/Service/ActionClipService.cs
@@ -21,6 +21,7 @@ namespace VF.Service {
     [VFService]
     public class ActionClipService {
         [VFAutowired] private readonly AvatarManager manager;
+        [VFAutowired] private readonly MutableManager mutableManager;
         [VFAutowired] private readonly AvatarManager avatarManager;
         [VFAutowired] private readonly ClipBuilderService clipBuilder;
         [VFAutowired] private readonly FullBodyEmoteService fullBodyEmoteService;
@@ -368,6 +369,25 @@ namespace VF.Service {
                 .Select(t => t.Value)
                 .DefaultIfEmpty(ShaderUtil.ShaderPropertyType.Float)
                 .First();
+
+            // this is a list of all distinct shader keywords for the materials
+            // and shaders currently on the renderers
+            var keywords = renderers
+                //Get all of the materials
+                .SelectMany( x => x.materials )
+                .Select( x => x.shader )
+                .SelectMany( x => x.keywordSpace.keywordNames )
+                .Distinct()
+                .ToList();
+
+            var toFind = propName.ToUpper() + "_ON";
+            var propIsKeyword = keywords.Contains(toFind);
+
+            if (propIsKeyword)
+            {
+                Debug.LogWarning($"Property \"{propName}\" is a shader keyword and may not animate correctly");
+            }
+
             return (renderers, type);
         }
 
